4dfb847ad6f4a0a6aaaa91cacdb586711476ac58
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2020-10-16 20:39:07 +0700

    Remove variable unused

--- config/config.ts
+++ config/config.ts
@@ -2,12 +2,10 @@ import nodeConfig from "config";
 
 interface Config {
   port: string;
-  verifyToken: string;
 }
 
 const config: Config = {
   port: nodeConfig.get<string>("port"),
-  verifyToken: nodeConfig.get<string>("verifyToken"),
 };
 
 export default config;
--- config/development.ts
+++ config/development.ts
@@ -4,5 +4,4 @@ dotenvFlow.config();
 
 module.exports = {
   port: process.env.PORT || 3000,
-  verifyToken: process.env.VERIFY_TOKEN || "VERIFY_TOKEN",
 };
